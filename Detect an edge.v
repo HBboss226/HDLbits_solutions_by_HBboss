346231828f1df8d2b3d78507e592c8e335144d74
HBboss226 <oyekalehabeeb26@gmail.com>
2026-09-12 22:30:38 +0100

    Update Detect an edge.v

--- Detect an edge.v
+++ Detect an edge.v
@@ -1,23 +1,15 @@
 module top_module (
     input clk,
     input [7:0] in,
-    output [7:0] pedge
+    output reg [7:0] pedge // To cut all intermediary registers, we make the pedge output a register itself
 );
-    // Here, I'm declaring my registers and loop variable. The can1 is short for my container of past values. out1 serves
-    //as a register to store the output but delay it by one clock cycle before it gets to out2 which is the real output
-    // because out2 is continiuosly assigned to pedge(teh output wire) 
-    reg [7:0] can1, out1, out2;
-    integer i;
+    // After consulting with gemini. I found this to be the better and faster solution
+    reg [7:0] cont;
     
     assign pedge = out2;
     always @(posedge clk)begin
-        can1 <= in;
-        out2 <= out1;
-        for (i=0 ; i < 8 ; i++)begin
-            if ((in[i]) & (!can1[i]))  out1[i] <= 1;
-            else if ((in[i]) & (can1[i])) out2[i] <= 0; // here, out2 is changed directly. if not, the output of 0 would be delayed by one clock cycle
-            else out2[i] <= 0;
-        end
-    end
+        in <= cont;
 
+        pedge <= in & ~cont;
+    end
 endmodule
